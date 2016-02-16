4285cc1a39f071a59962ddff5c57ec74d58686ff
Eric Conde <condeeric@live.com>
2016-02-16 15:18:42 +0800

    scoped main

---
+++ app/assets/javascripts/greet.jsx
@@ -0,0 +1,8 @@
+
+class Greet extends React.Component {
+  render () {
+    return (
+      <h1>Hello React, Flux, and Rails</h1>
+    )
+  }
+}
---
+++ app/assets/javascripts/main.jsx
@@ -0,0 +1,24 @@
+{
+  
+  class Main extends React.Component {
+    render() {
+      return (
+        <Greet />
+      )
+    }
+  }
+
+
+  let R = ReactDOM
+  let documentReady = () => {
+    R.render(
+      // <h1>Hello World</h1>, // <= render directly... or
+      <Main />, // render from a component (inline or separate file)
+      document.getElementById('react')
+
+    )
+  }
+
+  $(documentReady);
+
+}
