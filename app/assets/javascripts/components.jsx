b1bdc8397fcd018ed78bcf2b8b2266ede6f5e2fe
Eric Conde <condeeric@live.com>
2016-02-16 14:55:25 +0800

    prerender components

---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,20 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <h1>Hello from the component and some SEO content...</h1>
+    )
+  }
+}
+
+
+// let R = ReactDOM
+// let documentReady = () => {
+//   R.render(
+//     // <h1>Hello World</h1>, // <= render directly... or
+//     <Main />, // render from a component (inline or separate file)
+//     document.getElementById('react')
+//
+//   )
+// }
+//
+// $(documentReady);
