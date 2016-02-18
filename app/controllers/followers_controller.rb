2bd871cbf1079842095c5f8ae70c517e220dc2aa
Eric Conde <condeeric@live.com>
2016-02-18 23:47:38 +0800

    create follower endpoint

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -7,4 +7,21 @@ class FollowersController < ApplicationController
       f.json { render json: @users }
     end
   end
+
+  def create
+    @follower = Followers.create(follower_params)
+    respond_to do |f|
+      f.html
+      f.json {
+        render json: @follower, status: :created, location: follower_path(@follower)
+      }
+    end
+  end
+
+  private
+
+  def follower_params
+    params.require(:user).permit(:user_id)
+      .merge(:followed_by => current_user.id)
+  end
 end
