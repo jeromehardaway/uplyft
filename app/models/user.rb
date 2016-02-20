69baea3cf22f9df4b7aef9723c2f423c8472bdb5
Eric Conde <condeeric@live.com>
2016-02-20 21:16:03 +0800

    fix follow view render and json output

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
 
   has_many :tweets
 
-  # def as_json(options={})
-  #   {id: id, name: name}
-  # end
+  def as_json(options={})
+    {id: id, name: name, gravatar: gravatar}
+  end
 
   def name
     first_name.present? ? "#{first_name} #{last_name}" : email
@@ -18,5 +18,10 @@ class User < ActiveRecord::Base
     where(["id != :current_user_id", {current_user_id: current_user_id}])
     .order("random()")
     .all
+  end
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(email)
+    "http://www.gravatar.com/avatar/#{hash}"
   end
 end
